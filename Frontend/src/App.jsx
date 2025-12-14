909391a8df5873e045293bfb5557df6cad6808ef
Omar-Abdelrady1711 <Omarabdelrady019@gmail.com>
2025-12-14 18:27:28 -0500

    testing frontend autograder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
         }
       />
       
-      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
